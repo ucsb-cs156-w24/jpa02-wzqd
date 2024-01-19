f207206c579809f0db80f7128c7b23a1693b55fa
Jason Gu <1057337859@qq.com>
2024-01-18 18:45:04 -0800

    jpa02

--- src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from yisheng";
+        return "Greetings from wzqd";
     }
     
 }
